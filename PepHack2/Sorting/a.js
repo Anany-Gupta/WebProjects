f0e5605a00f246c3cd37839d4cc721cbe797b8c3
Anany-Gupta <ananygupta@gmail.com>
2021-07-23 16:33:32 +0530

    Completed PepHACK2

---
+++ PepHack2/Sorting/a.js
@@ -0,0 +1,65 @@
+
+
+document.querySelector(".mrg").addEventListener("click", async function () {
+    console.log(array);
+    await mergeSort(array, 0, array.length-1);
+    console.log(array);
+    displaySortedList(array);
+})
+function merge(arr, l, m, r) {
+    let n1 = m - l + 1;
+    let n2 = r - m;
+
+    let L = new Array(n1);
+    let R = new Array(n2);
+
+    for (let i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (let j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    
+    let i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+function mergeSort(arr, l, r) {
+    if (l >= r) {
+        return;//returns recursively
+    }
+    let m = l + parseInt((r - l) / 2);
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+}
+
+
+function displaySortedList(array) {
+
+}
